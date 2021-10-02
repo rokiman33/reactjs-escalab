29d72126acbebdee9fa72bbbfa9696efa93e6130
Richard Diaz <34225851+rokiman33@users.noreply.github.com>
2021-10-02 00:59:06 -0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiUrl = 'https://giftcard-escalab-project.herokuapp.com/'; //your api base url
-//const proxyurl = "https://thingproxy.freeboard.io/fetch/"; //proxy for local testing, remove this in production version
+const proxyurl = "https://thingproxy.freeboard.io/fetch/"; //proxy for local testing, remove this in production version
 
 const api = axios.create({
     baseURL:apiUrl,
